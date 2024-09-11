680c3dab76fc379ce9e15ef9f20ff2fe789c320e
Luis Cerqueira <dev.luiscerqueira@gmail.com>
2024-09-11 14:27:59 -0300

    11092024-1427

--- basica/index.js
+++ basica/index.js
@@ -1,4 +1,55 @@
-const { select } = require('@inquirer/prompts') // Ou seja, do objeto devolvido a partir do require, eu precisarei do 'select'.
+const { select, input, checkbox } = require('@inquirer/prompts') // Ou seja, do objeto devolvido a partir do require, eu precisarei do 'select'.
+
+//----------------------------------------------------------------------------------------------------------------------------
+
+let metas = [];
+
+// ---------------------------------------------------------------------------------------------------------------------------
+
+const cadastrarMeta  = async () => {
+    const meta = await input(
+        { message: "Digite a meta:" }
+    )
+
+    if(meta.length == 0){
+        console.log("A meta não pode ser vazia") // Perceba que se o usuário não digitou nada no input, o tamanho da const meta será zero e isso, lá no switch, não satisfaz a condição para sair e, portanto, a função start volta para o while e dá nova oportunidade do usuário tentar novamente.
+        return // Permitirá sair da função e continuar executando a função start
+    }
+
+    metas.push(
+        {
+            value: meta,
+            checked: false
+        }
+    ) // Caso o usuário tenha digitado algo, inserimos a meta dentro da listagem de metas com o atributo checked sendo false, sinalizando que a meta ainda não foi cumprida
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------
+
+const listarMetas = async () => {
+    const respostas = await checkbox(
+        {
+            message: "Use as setas para mudar de meta, o espaço para marcar e desmarcar e o enter para finalizar esta etapa",
+            choices: [...metas] // Aqui há um espalhamento "...", a criação de uma espécie de cópia do conteúdo do array de metas, visando que alterações realizadas não se consolidem imediatamente, mas sim, após satisfazerem as condições necessárias.
+        }
+    )
+
+    if(respostas.length == 0){
+        console.log("Nenhuma meta selecionada");
+        return
+    }
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true;
+    })
+
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------
 
 const start = async () => { // Ao usar o await, é preciso utilizar o async.
     while(true){
@@ -24,10 +75,11 @@ const start = async () => { // Ao usar o await, é preciso utilizar o async.
         
         switch(opcao){
             case "cadastrar":
-                console.log("Cadastrar Meta");
+                await cadastrarMeta(); // Porque também precisa aguardar
+                // console.log(metas)
                 break
             case "listar":
-                console.log("Listar Meta");
+                await listarMetas();
                 break
             case "sair":
                 return
